cbd32a05b922d233e8e82b73bcd6b7f7bde066e5
Godwin John <gj09042003@gmail.com>
2025-05-21 17:37:42 +0100

    Initialized sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,25 +1,28 @@
-import { defineConfig } from "sanity";
-import { deskTool } from "sanity/desk";
-import { visionTool } from "@sanity/vision";
-import { schemaTypes } from "./src/sanity/schemas";
-import StudioNavbar from "./src/components/StudioNavbar";
+'use client'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+/**
+ * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...tool]]/page.tsx` route
+ */
+
+import {visionTool} from '@sanity/vision'
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+
+// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
+import {apiVersion, dataset, projectId} from './src/sanity/env'
+import {schema} from './src/sanity/schemaTypes'
+import {structure} from './src/sanity/structure'
 
 export default defineConfig({
-  basePath: "/studio",
-  name: "Agrovestors_Studio",
-  title: "Agrovestors Studio",
+  basePath: '/studio',
   projectId,
   dataset,
-  plugins: [deskTool(), visionTool()],
-  schema: {
-    types: schemaTypes,
-  },
-  studio: {
-    components: {
-      navbar: StudioNavbar,
-    },
-  },
-});
+  // Add and edit the content schema in the './sanity/schemaTypes' folder
+  schema,
+  plugins: [
+    structureTool({structure}),
+    // Vision is for querying with GROQ from inside the Studio
+    // https://www.sanity.io/docs/the-vision-plugin
+    visionTool({defaultApiVersion: apiVersion}),
+  ],
+})
